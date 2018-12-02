3566785d30b702768ac46027689edccb59a13d30
chenminglin <806663326@qq.com>
2018-12-02 22:10:14 +0800

    完成底座椭圆绘制，完成梯形绘制。

--- app/src/main/java/com/bethena/cleanshortvideo/view/CleanShortVideoView.java
+++ app/src/main/java/com/bethena/cleanshortvideo/view/CleanShortVideoView.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Camera;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,14 +19,26 @@ import com.bethena.cleanshortvideo.R;
  */
 public class CleanShortVideoView extends View {
 
-    Paint paint;
+
     int mCenterX;
     int mCenterY;
     private Camera mCamera;
     private Matrix mMatrix;
 
 
+    Paint mRectPaint;
+    final int RECT_COLOR1 = 0x00FFFFFF;
+    final int RECT_COLOR2 = 0x22FFFFFF;
 
+
+    int mOvalWidthRaduis;
+    int mOvalHeightRaduis;
+
+    Paint mOvalPaint;
+    final int OVAL_COLOR1 = 0xFF5CD783;
+    RectF mOval1;
+    final int OVAL_COLOR2 = 0xFF0EC456;
+    RectF mOval2;
 
     public CleanShortVideoView(Context context) {
         super(context);
@@ -48,6 +60,11 @@ public class CleanShortVideoView extends View {
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.CleanShortVideoView, defStyle, 0);
 
+        a.recycle();
+
+        mCamera = new Camera();
+        mMatrix = new Matrix();
+
         initPaint();
     }
 
@@ -58,25 +75,38 @@ public class CleanShortVideoView extends View {
         mCenterX = w / 2;
         mCenterY = h / 3 * 2;
 
+        LinearGradient lg = new LinearGradient(0, -mCenterY, 0, 0, RECT_COLOR1, RECT_COLOR2, Shader.TileMode.CLAMP);
+        mRectPaint.setShader(lg);
+
+        mOvalWidthRaduis = w / 4;
+        mOvalHeightRaduis = h / 10;
+
+        mOval1 = new RectF();
+        mOval1.set(-mOvalWidthRaduis, -mOvalHeightRaduis - 10, mOvalWidthRaduis, mOvalHeightRaduis-10);
+        mOval2 = new RectF();
+        mOval2.set(-mOvalWidthRaduis, -mOvalHeightRaduis , mOvalWidthRaduis, mOvalHeightRaduis );
+//
+
     }
 
     private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(Color.WHITE);
-        paint.setStyle(Paint.Style.FILL);
+        mRectPaint = new Paint();
+        mRectPaint.setAntiAlias(true);
+        mRectPaint.setStyle(Paint.Style.FILL);
+
+        mOvalPaint = new Paint();
+        mOvalPaint.setAntiAlias(true);
+        mOvalPaint.setStyle(Paint.Style.FILL);
 
 
-        mCamera = new Camera();
-        mMatrix = new Matrix();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        LinearGradient lg = new LinearGradient(0, -200, 0, 0, Color.YELLOW, Color.WHITE, Shader.TileMode.CLAMP);
-        paint.setShader(lg);
+
         canvas.translate(mCenterX, mCenterY);
+
 
         canvas.save();
         mMatrix.reset();
@@ -85,10 +115,20 @@ public class CleanShortVideoView extends View {
         mCamera.getMatrix(mMatrix);
         mCamera.restore();
         canvas.concat(mMatrix);
+        canvas.drawRect(-mOvalWidthRaduis, -mCenterY, +mOvalWidthRaduis, 0, mRectPaint);
+        canvas.restore();
 
-        canvas.drawRect(-100, -200, +100, 0, paint);
+        canvas.save();
+        mOvalPaint.setColor(OVAL_COLOR2);
+        canvas.drawOval(mOval2, mOvalPaint);
+
+        mOvalPaint.setColor(OVAL_COLOR1);
+        canvas.drawOval(mOval1, mOvalPaint);
 
         canvas.restore();
+
+
+
 
     }
 
